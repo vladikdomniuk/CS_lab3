c66fd70bc1f0dd89d7371581a505bb17d62b6716
Vladyslav Domniuk <domnvo.vladik@gmail.com>
2021-04-14 16:35:39 +0300

    first commit

--- secondscr.sh
+++ secondscr.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+cd /home/grid/testbed/tb022/CS_lab3-main
+ml icc
 w1=( "sse2" "sse3" "sse4.1" "sse4.2" "avx")
 for p in "${w1[@]}";do
 icc -O -m$p c.cpp -o yra
 echo
 echo $p
-time `for i in {1..1000};do ./yra;done`
+time `for i in {1..1200000};do ./yra;done`
 done
 ~
