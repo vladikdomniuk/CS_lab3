1db76a401f1b521004410eda42596ece7b0dc6af
Vladyslav Domniuk <domnvo.vladik@gmail.com>
2021-04-15 23:48:42 +0300

    first commit

--- firstscr.sh
+++ firstscr.sh
@@ -2,7 +2,7 @@
 cd /home/grid/testbed/tb022/CS_lab3-main
 ml icc
 for O in {1..3};do
-scr=_$(date +%A_%H_%M_%S)
+scr=_$(date +%A_%H_%M_%S_%N)
 icc -O$O c.cpp -o try$scr
 echo
 echo "O"$O
--- secondscr.sh
+++ secondscr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/grid/testbed/tb022/CS_lab3-main
 ml icc
-scr=$(date+%A_%H_%M_%S)
+scr=_$(date +%A_%H_%M_%S_%N)
 w1=( "sse2" "sse3" "sse4.1" "sse4.2" "avx")
 for p in "${w1[@]}";do
 icc -O -m$p c.cpp -o yra$scr
